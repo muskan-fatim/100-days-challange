0cc4a4837436bd1cee97860c9a0f5f99ac2ade50
muskan-fatim <160476952+muskan-fatim@users.noreply.github.com>
2024-03-04 00:09:36 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//question no2 
+var message = "Muskan would you like to learn some typscript?";
+console.log(message);
+//question no 3
+var person_name = "Muskan";
+var person = person_name.toUpperCase();
+var person2 = person_name.toLowerCase();
+console.log(person);
+console.log(person2);
---
+++ index.ts
@@ -0,0 +1,9 @@
+//question no2 
+let message :string = "Muskan would you like to learn some typscript?"
+console.log(message)
+//question no 3
+let person_name: string = "Muskan"
+let person = person_name.toUpperCase()
+let person2 = person_name.toLowerCase()
+console.log(person)
+console.log(person2)
